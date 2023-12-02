2390d4f4012e88bf755c12c8980b7e38435ee96f
Amsurur”git config --global user.name Amsurur” <abdulloevmuhammadsurur@gmail.com>
2023-12-02 11:17:20 +0300

    Last

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,31 @@
+
+
 <script setup>
 
 </script>
 
 <template>
+  
   <div>
 <div><p class="text-center text-4xl mt-4 font-bold font-mono">{{ title }}</p></div>
 
-<div class="todo">
-  <input type="" v-model="text" placeholder="todo" class="outline-none border">
-  <button @click="addTodo(id++)">add todo</button>
-  <div class="bg-gray-100 w-[300px]" v-for="(todo,) in todos" :key="todo.id">
-    <p class="flex gap-10">  
-      <span class="todo__id">{{ todo.id +1 }}</span>
+<div class="">
+  <div class="justify-center mt-10 flex gap-5">
+  <input type="" v-model="text" placeholder="I can do" class="outline-none w-[420px] h-[40px] rounded-lg px-3 border border-green-500 ">
+  <button @click="addTodo(id++)" class="bg-green-400 w-[100px] text-white h-[40px] rounded-lg" >Add</button>
+</div>
+  <div class="bg-gray-100  border-red-400 border rounded-lg px-2 w-[35%]  mt-2 mx-auto  " v-for="(todo,) in todos" :key="todo.id">
+    <div class="flex justify-center items-center p-2 gap-10 ">  
+      <span class="todo__id bg-orange-300  text-center px-2  rounded-xl"  >{{ todo.id }}</span>
   
       <span  v-if="!editting"  class="todo__text " :class="{todo__text_isDone:isDone}" >{{ todo.text }}</span>
       <input v-bind:value="editText" @change="editTextChange" v-else type="text">
+      <div  class="flex items-center gap-2">
       <input type="checkbox" class="inline-block todo_check" v-model="isDone" >
-      <button @click="deleteTodo(todo.id)">delete</button>
-      <button @click="editTodo(todo)">{{editting?"update":"edit"}}</button>
-    
-    </p>
+      <button @click="deleteTodo(todo.id)" class="bg-red-400 px-2 text-white rounded">delete</button>
+      <button @click="editTodo(todo)" class="bg-purple-400 px-2 text-white rounded">{{editting?"update":"edit"}}</button>
+    </div>
+    </div>
  
 
   </div>
@@ -32,7 +38,7 @@ import {mapActions} from "vuex"
 export default {
   data(){
     return {
-      title:"Todo app",
+      title:"Todo List",
       text:"",
       todos:[],
       isDone:false,
@@ -44,12 +50,18 @@ export default {
   methods:{
     ...mapActions(["deleteTodo","editTodo"]),
     addTodo(){
-      this.todos.push({
+      if(this.text.trim().length == 0){
+        alert("please add todo")
+      }
+      else{
+           this.todos.push({
         id:this.id,
         text:this.text,
         done:this.isDone
     }),
-      this.todo=""
+      this.text=""
+      }
+   
     },
     deleteTodo(id){
       console.log(id);
